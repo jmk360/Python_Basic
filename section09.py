daddfce322ce7dda268a81b60e1f4d88458d5a61
JungMo Kim <jmk360@daum.net>
2021-12-14 11:43:12 +0900

    파일 읽기, 쓰기

---
+++ section09.py
@@ -0,0 +1,100 @@
+# Section09
+# 파일 읽기, 쓰기
+# 읽기 모드 : r, 쓰기 모드(기존 파일 삭제) : w, 추가 모드(파일 생성 또는 추가) : a
+# .. : 상대 경로, . : 절대 경로
+
+# 파일 읽기
+# 예제1
+f = open('./resource/review.txt', 'r')
+content = f.read()
+print(content)
+print(dir(f))
+# 반드시 close 리소스 반환
+f.close()
+
+print('-'*100)
+
+# 예제2
+with open('resource/review.txt', 'r') as f:
+    c = f.read()
+    print(c)
+    print(list(c))
+    print(iter(c))
+
+print('-'*100)
+print('-'*100)
+
+# 예제3
+with open('resource/review.txt', 'r') as f:
+    for c in f:
+        print(c.strip())
+
+print('-'*100)
+print('-'*100)
+
+# 예제4
+with open('resource/review.txt', 'r') as f:
+    content = f.read()
+    print(">", content)
+    content = f.read() # 내용 없음
+    print(">", content)
+    
+print('-'*100)
+print('-'*100)
+
+# 예제5
+with open('resource/review.txt', 'r') as f:
+    line = f.readline()
+    # print(line)
+    while line:
+        print(line.strip()) 
+        line = f.readline()
+
+print('-'*100)
+print('-'*100)
+
+# 예제6
+with open('resource/review.txt', 'r') as f:
+    content = f.readlines()
+    print(content)
+    for c in content:
+        print(c, end = ' ***** ')
+
+print('-'*100)
+print('-'*100)
+
+# 예제7
+score = []
+with open('./resource/score.txt', 'r') as f:
+    for line in f:
+        score.append(int(line))
+    print(score)
+print('Average : {:6.3}'.format(sum(score) / len(score)))
+
+# 파일 쓰기
+# 예제1
+with open('./resource/text1.txt', 'w') as f:
+    f.write('Niceman!!\n')
+
+# 예제2
+with open('./resource/text1.txt', 'a') as f:
+    f.write('Good man!\n')
+
+# 예제3
+from random import randint
+
+with open('./resource/text2.txt', 'w') as f:
+    for cnt in range(6):
+        f.write(str(randint(1, 50)))
+        f.write('\n')
+
+# 예제4
+# writelines : 리스트 -> 파일로 저장
+with open('./resource/text3.txt', 'w') as f:
+    list = ['kim\n', 'park\n', 'cho\n']
+    f.writelines(list)
+
+# 예제5
+with open('./resource/text4.txt', 'w') as f:
+    print('Test Contests1!', file=f)
+    print('Test Contests2!', file=f)
